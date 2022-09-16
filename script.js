a9606ebd921999158459be040b1a02bf5ebe68b7
iamvijaysubramanian <108535368+iamvijaysubramanian@users.noreply.github.com>
2022-09-16 20:10:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+var ele=document.createElement("h1");
+// function bar(){
+    setTimeout(() => {
+        ele.innerHTML="10";
+    setTimeout(()=>{
+        ele.innerHTML="9";
+    setTimeout(()=>{
+        ele.innerHTML="8";
+    setTimeout(()=>{
+        ele.innerHTML="7";
+    setTimeout(()=>{
+        ele.innerHTML="6";
+    setTimeout(()=>{
+        ele.innerHTML="5";
+    setTimeout(()=>{
+        ele.innerHTML="4";
+    setTimeout(()=>{
+        ele.innerHTML="3";
+    setTimeout(()=>{
+        ele.innerHTML="2";
+    setTimeout(()=>{
+        ele.innerHTML="1";
+    setTimeout(()=>{
+        ele.innerHTML="Happy Independance Day";
+     });
+     },1000)
+     },1000)
+    },1000)
+    },1000)
+    },1000)
+    },1000)
+    },1000)
+    },1000)
+    },1000)
+}, 1000)
+// bar();
+document.body.append(ele);
+// }
